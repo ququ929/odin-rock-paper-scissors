59e54a437339c0002ca137f9d6bfa10f171974de
ququ929 <ququ929@hotmail.com>
2022-10-12 19:40:44 +0800

    Create JS rock-paper-scissors game console version, play only in console.

---
+++ js/rock.js
@@ -0,0 +1,39 @@
+// rock, paper or scissors, return one of these randomly.
+function getComputerChoice() {
+    let rockPaperScissors = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    const randomResult = rockPaperScissors[randomIndex];
+    return randomResult;
+}
+
+// play one game of rock-paper-scissors
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "Tie game!";
+    }
+
+    else if (
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock")) {
+            return "You win!";
+        }
+
+    else {
+        return "You lose!";
+    }
+}
+
+
+// play the game 5 times, with user input.
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper or Scissors? ")
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
